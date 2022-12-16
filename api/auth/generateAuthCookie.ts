4794db642203b331984a62cd59442983012f799b
beebls <logantkordov@gmail.com>
2022-12-16 14:45:24 -0700

    add domain to cookie

--- api/auth/generateAuthCookie.ts
+++ api/auth/generateAuthCookie.ts
@@ -1,7 +1,9 @@
 export function generateAuthCookie(token: string) {
   const d = new Date();
   d.setTime(d.getTime() + (7 * 24 - 1) * 60 * 60 * 1000);
-  document.cookie = `authToken=${token}; expires=${d.toUTCString()}; secure=true; SameSite=none;`;
+  document.cookie = `authToken=${token}; expires=${d.toUTCString()}; secure=true; SameSite=none; domain=${
+    process.env.NEXT_PUBLIC_COOKIE_DOMAIN
+  };`;
   localStorage.tokenExpiryDate = new Date().valueOf() + 60 * 10 * 1000;
   return true;
 }
