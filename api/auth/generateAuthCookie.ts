c4a5ea798514a5797242d7d42f3d4e24ce87ad9e
beebls <logantkordov@gmail.com>
2022-12-17 12:48:35 -0700

    Make auth cookie no longer cross-site

--- api/auth/generateAuthCookie.ts
+++ api/auth/generateAuthCookie.ts
@@ -2,11 +2,11 @@ export function generateAuthCookie(token: string) {
   if (token) {
     const d = new Date();
     d.setTime(d.getTime() + (7 * 24 - 1) * 60 * 60 * 1000);
-    document.cookie = `authToken=${token}; expires=${d.toUTCString()}; SameSite=none; path=/; ${
+    document.cookie = `authToken=${token}; expires=${d.toUTCString()}; SameSite=Lax; path=/; ${
       process.env.NEXT_PUBLIC_DEV_MODE === "true"
         ? ""
         : `domain=${process.env.NEXT_PUBLIC_COOKIE_DOMAIN};`
-    }`;
+    } Secure`;
     localStorage.tokenExpiryDate = new Date().valueOf() + 60 * 10 * 1000;
   } else {
     console.log("NO TOKEN TO CREATE COOKIE WITH");
