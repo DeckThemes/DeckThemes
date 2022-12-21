554d4695a262c355be88900802c7836672d1489f
beebls <logantkordov@gmail.com>
2022-12-20 18:42:56 -0700

    FilePond is my daddy 🤤🤤
    zip/img uploading should be much much smoother now

--- pages/submit.tsx
+++ pages/submit.tsx
@@ -1,28 +1,31 @@
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useState, Dispatch, SetStateAction, useEffect } from "react";
-import {
-  BsFillTrashFill,
-  BsArrowUp,
-  BsArrowDown,
-  BsFolderFill,
-  BsCloudUploadFill,
-  BsXLg,
-} from "react-icons/bs";
+
+// @ts-ignore
+import { FilePond, File, registerPlugin } from "react-filepond";
+import "filepond/dist/filepond.min.css";
+import FilePondPluginImagePreview from "filepond-plugin-image-preview";
+import "filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css";
+import FilePondPluginImageExifOrientation from "filepond-plugin-image-exif-orientation";
+import FilePondPluginFileValidateSize from "filepond-plugin-file-validate-size";
+import FilePondPluginFileValidateType from "filepond-plugin-file-validate-type";
+
 import { checkAndRefreshToken, genericGET } from "../api";
 
-import {
-  APIBlob,
-  CSSSubmissionInfo,
-  GitSubmissionInfo,
-  MetaInfo,
-  ZipSubmissionInfo,
-} from "../types";
+import { CSSSubmissionInfo, GitSubmissionInfo, MetaInfo, ZipSubmissionInfo } from "../types";
+registerPlugin(
+  FilePondPluginFileValidateSize,
+  FilePondPluginFileValidateType,
+  FilePondPluginImageExifOrientation,
+  FilePondPluginImagePreview
+);
 
 const sectionContainerClasses = "flex flex-col w-full items-center gap-4";
 
 const fieldClasses = "bg-bgLight dark:bg-bgDark p-2 rounded-3xl w-full";
 const fieldContainerClasses = "flex flex-col md:flex-row w-full md:w-1/2 items-center";
+const metaFieldContainerClasses = "flex flex-col gap-2 w-full md:w-1/2 items-center";
 const fieldTitleClasses =
   "bg-borderLight dark:bg-borderDark px-2 py-1 rounded-2xl text-medium text-xl mb-2 md:mb-0 md:mr-2";
 
@@ -89,6 +92,7 @@ export default function Submit() {
       description: metaInfo.description || null,
       target: metaInfo.target !== "None" ? metaInfo.target : null,
     };
+    console.log(data(), formattedMeta);
     const waitForRefresh = await checkAndRefreshToken();
     if (waitForRefresh) {
       fetch(`${process.env.NEXT_PUBLIC_API_URL}/submissions/${uploadType}_${uploadMethod}`, {
@@ -127,6 +131,17 @@ export default function Submit() {
       <Head>
         <title>DeckThemes | Submit</title>
       </Head>
+      <style>
+        {`
+        
+        .dark .filepond--panel-root {
+          background-color: #2e2e2e;
+        }
+        .dark .filepond--drop-label {
+          color: #fff;
+        }
+        `}
+      </style>
       <div className="flex flex-col items-center w-full grow text-center gap-4 pt-4">
         <h1 className="text-3xl md:text-4xl font-semibold py-4">Submit A Theme</h1>
         <div className="w-fit px-4 flex justify-center items-center bg-cardLight dark:bg-cardDark rounded-3xl mb-4 text-3xl">
@@ -240,75 +255,7 @@ function MetaPanel({
   uploadType?: "css" | "audio";
   uploadMethod?: string;
 }) {
-  const [image, setImage] = useState<File>();
-
-  // Since the info object is only the data that will be sent to the API, this stores image names so that it can be displayed to the frontend and so users can see what images are currently staged for submission
-  const [complimentaryImageInfo, setCompImageInfo] = useState<File[]>([]);
-
-  // I'm lazy
-  function onImageSelect(event: any) {
-    setImage(event?.target?.files[0] || undefined);
-  }
-
-  async function uploadImage() {
-    if (image) {
-      if (image.type !== "image/jpeg") {
-        alert("Image must be a JPEG file");
-        return;
-      }
-      if (image.size >= 510000) {
-        alert("Image must be smaller than 0.5MB");
-        return;
-      }
-
-      // The blob upload takes a formData object as it's body
-      const formData = new FormData();
-      formData.append("File", image);
-
-      const waitForRefresh = await checkAndRefreshToken();
-      if (waitForRefresh) {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/blobs`, {
-          method: "POST",
-          body: formData,
-          credentials: "include",
-        })
-          .then((res) => {
-            console.log(res);
-            return res.json();
-          })
-          .then((json: APIBlob) => {
-            if (json?.id) {
-              setInfo({ ...info, imageBlobs: [...info.imageBlobs, json.id] });
-              setCompImageInfo([...complimentaryImageInfo, image]);
-              setImage(undefined);
-            }
-          });
-      }
-    } else {
-      alert("No Image Attached");
-    }
-  }
-
-  function deleteImage(i: number) {
-    let blobData = [...info.imageBlobs];
-    let fileData = [...complimentaryImageInfo];
-    blobData.splice(i, 1);
-    fileData.splice(i, 1);
-    setInfo({ ...info, imageBlobs: blobData });
-    setCompImageInfo(fileData);
-  }
-
-  function moveImage(from: number, to: number) {
-    let blobData = [...info.imageBlobs];
-    let fileData = [...complimentaryImageInfo];
-    const blob = blobData.splice(from, 1)[0];
-    const file = fileData.splice(from, 1)[0];
-    blobData.splice(to, 0, blob);
-    fileData.splice(to, 0, file);
-
-    setInfo({ ...info, imageBlobs: blobData });
-    setCompImageInfo(fileData);
-  }
+  const [images, setImages] = useState<File>();
 
   const [targetOptions, setTargetOptions] = useState<string[]>(["None", "System-Wide", "Snippet"]);
   async function getTargets() {
@@ -331,7 +278,7 @@ function MetaPanel({
   return (
     <>
       <div className={`${sectionContainerClasses}`}>
-        <div className={`${fieldContainerClasses}`}>
+        <div className={`${metaFieldContainerClasses}`}>
           <span className={`${fieldTitleClasses}`}>Target</span>
           <div className="flex justify-center w-full">
             <select
@@ -350,105 +297,77 @@ function MetaPanel({
           </div>
         </div>
         <MiniDivider />
-        <div className={`${fieldContainerClasses}`}>
+        <div className={`${metaFieldContainerClasses}`}>
           <span className={`${fieldTitleClasses}`}>Images</span>
-
           <div className="flex flex-col w-full relative">
-            <div className="flex items-center w-full justify-center">
-              {!!image ? (
-                <>
-                  {/* Idk why but keeping this outer-div helps stretch the things height-wise, so I'ma leave it */}
-                  <div className="flex w-full justify-center">
-                    <button
-                      onClick={uploadImage}
-                      className="flex items-center text-xl w-1/2 p-1 bg-bgLight dark:bg-bgDark rounded-2xl"
-                    >
-                      <BsCloudUploadFill className="mx-2 w-12" />
-                      <span className="w-64 truncate">
-                        <span className="text-blue-500">Upload </span>
-                        {image.name}
-                      </span>
-                    </button>
-                    <button
-                      className="flex items-center text-xl px-2 p-1 bg-bgLight dark:bg-bgDark rounded-2xl gap-2"
-                      onClick={() => {
-                        setImage(undefined);
-                      }}
-                    >
-                      <BsXLg className="text-red-700" />
-                    </button>
-                  </div>
-                </>
-              ) : (
-                <>
-                  <label
-                    htmlFor="img-upload"
-                    className="cursor-pointer flex items-center text-xl gap-2 bg-bgLight dark:bg-bgDark p-1 px-2 rounded-2xl"
-                  >
-                    <BsFolderFill />
-                    <span className="font-medium">Pick Image</span>
-                  </label>
-                  <input
-                    id="img-upload"
-                    type="file"
-                    accept="image/jpeg"
-                    onChange={onImageSelect}
-                    className="hidden"
-                  />
-                </>
-              )}
-            </div>
-            <ol className="flex flex-col w-full">
-              {complimentaryImageInfo.map((e, i, arr) => {
-                return (
-                  <li className="flex items-center mt-4" key={`Image ${i}`}>
-                    <span
-                      className={`text-medium text-2xl w-4 ${arr.length === 1 ? "pb-1 mr-4" : ""}`}
-                    >
-                      {i + 1}
-                    </span>
-                    {arr.length > 1 && (
-                      <>
-                        <div className="border-4 rounded-2xl border-borderLight dark:border-borderDark pt-1 w-16 mx-4">
-                          {i < arr.length - 1 && (
-                            <button
-                              onClick={() => {
-                                moveImage(i, i + 1);
-                              }}
-                            >
-                              <BsArrowDown size={25} />
-                            </button>
-                          )}
-
-                          {i > 0 && (
-                            <button
-                              onClick={() => {
-                                moveImage(i, i - 1);
-                              }}
-                            >
-                              <BsArrowUp size={25} />
-                            </button>
-                          )}
-                        </div>
-                      </>
-                    )}
-                    <span className="text-xl truncate w-64">{e.name}</span>
-                    <button
-                      className="ml-auto"
-                      onClick={() => {
-                        deleteImage(i);
-                      }}
-                    >
-                      <BsFillTrashFill size={25} className="text-red-700" />
-                    </button>
-                  </li>
-                );
-              })}
-            </ol>
+            <FilePond
+              allowFileTypeValidation
+              labelFileTypeNotAllowed="Invalid File Type!"
+              fileValidateTypeLabelExpectedTypes="Images must be in .JPG format"
+              acceptedFileTypes={["image/jpeg"]}
+              allowFileSizeValidation
+              allowImagePreview
+              allowImageExifOrientation
+              maxFileSize={process.env.NEXT_PUBLIC_MAX_IMG_SIZE}
+              files={images}
+              onupdatefiles={setImages}
+              onremovefile={(_, file) => {
+                setInfo({
+                  ...info,
+                  imageBlobs: info.imageBlobs.filter((e) => e !== file.serverId),
+                });
+              }}
+              onreorderfiles={(files) => {
+                setInfo({ ...info, imageBlobs: files.map((e) => e.serverId) });
+              }}
+              allowReorder
+              allowMultiple
+              maxFiles={5}
+              server={{
+                url: `${process.env.NEXT_PUBLIC_API_URL}/blobs`,
+                revert: (_, load) => {
+                  setInfo({ ...info, imageBlobs: [] });
+                  load();
+                },
+                process: (_, file, __, load, error) => {
+                  const formData = new FormData();
+                  formData.append("File", file);
+                  checkAndRefreshToken().then((bool) => {
+                    if (bool) {
+                      fetch(`${process.env.NEXT_PUBLIC_API_URL}/blobs`, {
+                        method: "POST",
+                        body: formData,
+                        credentials: "include",
+                      })
+                        .then((res) => {
+                          if (res.status >= 200 && res.status <= 300) {
+                            return res.json();
+                          }
+                          error("Res Not OK!");
+                        })
+                        .then((json) => {
+                          if (json?.id) {
+                            setInfo((info) => ({
+                              ...info,
+                              imageBlobs: [json.id, ...info.imageBlobs],
+                            }));
+                            load(json.id);
+                          }
+                        })
+                        .catch((err) => {
+                          error(err);
+                        });
+                    }
+                  });
+                },
+              }}
+              name="File"
+              labelIdle='Drag & Drop images or <span class="filepond--label-action">Browse</span>'
+            />
           </div>
         </div>
         <MiniDivider />
-        <div className={`${fieldContainerClasses}`}>
+        <div className={`${metaFieldContainerClasses}`}>
           <span className={`${fieldTitleClasses}`}>Description</span>
           <textarea
             value={info.description}
@@ -516,119 +435,68 @@ function ZipSubmitPanel({
   setInfo: Dispatch<SetStateAction<ZipSubmissionInfo>>;
 }) {
   const [file, setFile] = useState<File>();
-  const [uploadedFile, setUploadedFile] = useState<File>();
-
-  function onFileSelect(event: any) {
-    setFile(event?.target?.files[0] || undefined);
-  }
-
-  async function uploadFile() {
-    if (file) {
-      if (file.type !== "application/x-zip-compressed") {
-        alert("Must be a ZIP file");
-        return;
-      }
-      if (file.size >= 510000) {
-        alert("File must be smaller than 10MB");
-        return;
-      }
-
-      // The blob upload takes a formData object as it's body
-      const formData = new FormData();
-      formData.append("File", file);
-
-      const waitForRefresh = await checkAndRefreshToken();
-      if (waitForRefresh) {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/blobs`, {
-          method: "POST",
-          body: formData,
-          credentials: "include",
-        })
-          .then((res) => {
-            return res.json();
-          })
-          .then((json: APIBlob) => {
-            if (json?.id) {
-              setInfo({ ...info, blob: json.id });
-              setUploadedFile(file);
-              setFile(undefined);
-            }
-          });
-      }
-    } else {
-      alert("No Image Attached");
-    }
-  }
-
-  function deleteFile() {
-    setInfo({ blob: "" });
-    setUploadedFile(undefined);
-  }
 
   return (
     <>
       <div className="flex flex-col items-center w-full justify-center">
-        {!uploadedFile ? (
-          <>
-            {!!file ? (
-              <>
-                {/* Idk why but keeping this outer-div helps stretch the things height-wise, so I'ma leave it */}
-                <div className="flex w-[80vw] lg:w-[640px] justify-center">
-                  <button
-                    onClick={uploadFile}
-                    className="flex items-center text-xl w-1/2 p-1 bg-bgLight dark:bg-bgDark rounded-2xl"
-                  >
-                    <BsCloudUploadFill className="mx-2 w-12" />
-                    <span className="w-64 truncate">
-                      <span className="text-blue-500">Upload </span>
-                      {file.name}
-                    </span>
-                  </button>
-                  <button
-                    className="flex items-center text-xl px-2 p-1 bg-bgLight dark:bg-bgDark rounded-2xl gap-2"
-                    onClick={() => {
-                      setFile(undefined);
-                    }}
-                  >
-                    <BsXLg className="text-red-700" />
-                  </button>
-                </div>
-              </>
-            ) : (
-              <>
-                <label
-                  htmlFor="zip-upload"
-                  className="cursor-pointer flex items-center text-xl gap-2 bg-bgLight dark:bg-bgDark p-1 px-2 rounded-2xl"
-                >
-                  <BsFolderFill />
-                  <span className="font-medium">Pick File</span>
-                </label>
-                <input
-                  id="zip-upload"
-                  type="file"
-                  accept=".zip"
-                  onChange={onFileSelect}
-                  className="hidden"
-                />
-              </>
-            )}
-          </>
-        ) : (
-          <>
-            <div className="flex items-center">
-              <span className="text-xl font-semibold mr-2">Chosen Zip: </span>
-              <span className="text-xl truncate w-64">{uploadedFile.name}</span>
-              <button
-                className="ml-auto"
-                onClick={() => {
-                  deleteFile();
-                }}
-              >
-                <BsFillTrashFill size={25} className="text-red-700" />
-              </button>
-            </div>
-          </>
-        )}
+        <div className="w-full md:w-1/2">
+          <FilePond
+            allowFileTypeValidation
+            labelFileTypeNotAllowed="Invalid File Type!"
+            fileValidateTypeLabelExpectedTypes="File must be a .zip archive"
+            acceptedFileTypes={[
+              "application/zip",
+              "application/x-zip",
+              "application/x-zip-compressed",
+            ]}
+            allowFileSizeValidation
+            maxFileSize={process.env.NEXT_PUBLIC_MAX_ZIP_SIZE}
+            files={file}
+            onupdatefiles={setFile}
+            onremovefile={() => {
+              setInfo({ blob: "" });
+            }}
+            allowMultiple={false}
+            maxFiles={1}
+            server={{
+              url: `${process.env.NEXT_PUBLIC_API_URL}/blobs`,
+              // revert is fired on a deletion, but we dont need to delete anything on the server
+              // The actual delete logic is in onremovefile
+              revert: () => {},
+              process: (_, file, __, load, error) => {
+                const formData = new FormData();
+                formData.append("File", file);
+
+                checkAndRefreshToken().then((bool) => {
+                  if (bool) {
+                    fetch(`${process.env.NEXT_PUBLIC_API_URL}/blobs`, {
+                      method: "POST",
+                      body: formData,
+                      credentials: "include",
+                    })
+                      .then((res) => {
+                        if (res.status >= 200 && res.status <= 300) {
+                          return res.json();
+                        }
+                        error("Res Not OK!");
+                      })
+                      .then((json) => {
+                        if (json?.id) {
+                          setInfo({ blob: json.id });
+                          load(json.id);
+                        }
+                      })
+                      .catch((err) => {
+                        error(err);
+                      });
+                  }
+                });
+              },
+            }}
+            name="File"
+            labelIdle='Drag & Drop your files or <span class="filepond--label-action">Browse</span>'
+          />
+        </div>
       </div>
     </>
   );
