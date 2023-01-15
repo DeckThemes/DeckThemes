e7f3b1cae8b5d4232898be5ea56794c535ecda40
beebls <logantkordov@gmail.com>
2023-01-14 18:23:32 -0700

    add sitemap generator test

---
+++ sitemapGenerator.ts
@@ -0,0 +1,10 @@
+const sitemap = require("nextjs-sitemap-generator");
+sitemap({
+  baseUrl: process.env.NEXT_PUBLIC_CLIENT_URL || "",
+  ignoredPaths: ["submissions"],
+  pagesDirectory: __dirname + "\\.vercel/output/static/",
+  targetDirectory: __dirname + "\\.vercel/output/static/",
+  sitemapFilename: "sitemap.xml",
+  allowFileExtensions: true,
+  nextConfigPath: __dirname + "\\next.config.js",
+});
