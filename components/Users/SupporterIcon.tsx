a56b1a578ce5a6e0420a44f3a4a3c453354e3f27
Logan <logantkordov@gmail.com>
2023-03-09 08:54:28 -0700

    add gradient icon
    doing this from laptop with no testing env, so pushing to beta and seeing how it works

--- components/Users/SupporterIcon.tsx
+++ components/Users/SupporterIcon.tsx
@@ -7,16 +7,31 @@ export function SupporterIcon({ author }: { author: UserInfo }) {
     <>
       {author?.premiumTier !== "None" && (
         <>
-          <RiMedalFill
-            id="supporter-tt"
-            color={
-              author?.premiumTier == "Tier3"
-                ? "#FCC200"
-                : author?.premiumTier == "Tier2"
-                ? "#B4B4B4"
-                : "#805D39"
-            }
-          />
+          <svg width="0" height="0">
+            <linearGradient id="medal-gradient" x1="100%" y1="100%" x2="0%" y2="0%">
+              <stop
+                stopColor={
+                  author?.premiumTier == "Tier3"
+                    ? "#de2cf7"
+                    : author?.premiumTier == "Tier2"
+                    ? "#FCC200"
+                    : "#B4B4B4"
+                }
+                offset="0%"
+              />
+              <stop
+                stopColor={
+                  author?.premiumTier == "Tier3"
+                    ? "rgb(26,159,255)"
+                    : author?.premiumTier == "Tier2"
+                    ? "#FCC200"
+                    : "#B4B4B4"
+                }
+                offset="100%"
+              />
+            </linearGradient>
+          </svg>
+          <RiMedalFill id="supporter-tt" style={{ stroke: "url(#medal-gradient)" }} />
           <Tooltip
             anchorSelect="#supporter-tt"
             content={`Tier ${author?.premiumTier?.slice(-1)} Patreon Supporter`}
