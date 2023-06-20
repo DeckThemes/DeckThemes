c6b1847cbdfae192bb997da224a5289f8289c520
Evan / Fero <hi@evan.graphics>
2023-06-20 15:12:31 -0700

    init: prettier-plugin-tailwindcss for automatic class sorting

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+module.exprots = {
+	plugins: [require('prettier-plugin-tailwindcss')],
+	"singleQuote": false,
+	"tabWidth": 2,
+	"semi": true,
+	"trailingComma": "es5",
+	"bracketSameLine": false,
+	"printWidth": 100,
+	"quoteProps": "as-needed",
+	"bracketSpacing": true,
+	"arrowParens": "always"
+  }
+  
