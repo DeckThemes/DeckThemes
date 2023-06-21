db35a71c5737c644a266ba4f1af53e2f3c5de354
beebls <102569435+beebls@users.noreply.github.com>
2023-06-20 21:48:00 -0600

    fix prettier config

--- prettier.config.js
+++ prettier.config.js
@@ -1,13 +1,12 @@
-module.exprots = {
-	plugins: [require('prettier-plugin-tailwindcss')],
-	"singleQuote": false,
-	"tabWidth": 2,
-	"semi": true,
-	"trailingComma": "es5",
-	"bracketSameLine": false,
-	"printWidth": 100,
-	"quoteProps": "as-needed",
-	"bracketSpacing": true,
-	"arrowParens": "always"
-  }
-  
+module.exports = {
+  plugins: [require("prettier-plugin-tailwindcss")],
+  singleQuote: false,
+  tabWidth: 2,
+  semi: true,
+  trailingComma: "es5",
+  bracketSameLine: false,
+  printWidth: 100,
+  quoteProps: "as-needed",
+  bracketSpacing: true,
+  arrowParens: "always",
+};
