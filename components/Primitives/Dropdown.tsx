6c473ac82a7da33ccd8ac89609546d264a0872b0
Evan / Fero <hi@evan.graphics>
2023-07-03 12:12:41 -0700

    fix(WEB-3): attempt to fix dropdown triggering when scrolling on mobile

--- components/Primitives/Dropdown.tsx
+++ components/Primitives/Dropdown.tsx
@@ -1,5 +1,5 @@
 import * as DropdownMenu from "@radix-ui/react-dropdown-menu";
-import { useContext, useMemo } from "react";
+import { useContext, useMemo, useState } from "react";
 import { useTheme } from "next-themes";
 import { MdKeyboardArrowDown } from "react-icons/md";
 import { twMerge } from "tailwind-merge";
@@ -64,11 +64,24 @@ export function RadioDropdown({
     () => formattedOptions.find((e: any) => e.value === value) || formattedOptions[0],
     [formattedOptions, value]
   );
+
+  const [open, setOpen] = useState(false);
+
   return (
-    <DropdownMenu.Root>
+    <DropdownMenu.Root open={open} onOpenChange={setOpen}>
       <DropdownMenu.Trigger
         aria-label={ariaLabel}
         className="flex w-full select-none flex-col gap-2"
+        onPointerUp={(e) => {
+          if (e.pointerType === "touch" || e.pointerType === "pen") {
+            setOpen(true);
+          }
+        }}
+        onPointerDown={(e) => {
+          if (e.pointerType === "touch" || e.pointerType === "pen") {
+            e.preventDefault();
+          }
+        }}
       >
         {headingText && (
           <span className={twMerge("w-full text-left font-bold", headingClass)}>{headingText}</span>
@@ -95,45 +108,47 @@ export function RadioDropdown({
       </DropdownMenu.Trigger>
 
       <DropdownMenu.Portal>
-        {/* hot take, i actually think that forcing the dropdowns to be in dark mode has better contrast */}
-        <div className={`${resolvedTheme} contents`}>
-          {/* bg-base-3-light dark:bg-base-3-dark w-[250px] text-black dark:text-white rounded-xl border-2 border-borders-base2-light dark:border-borders-base2-dark  */}
-          <DropdownMenu.Content
-            avoidCollisions
-            collisionPadding={16}
-            className="font-fancy radio-dropdown z-[9999] my-1 h-max w-[250px] cursor-default select-none overflow-hidden overflow-y-auto rounded-xl bg-base-3-light text-sm text-black transition-all dark:bg-base-3-dark dark:text-white"
-          >
-            <DropdownMenu.RadioGroup value={value} onValueChange={onValueChange}>
-              <div className="max-h-[var(--radix-popper-available-height)]">
-                {formattedOptions.map((e) => (
-                  <DropdownMenu.RadioItem
-                    disabled={e.disabled}
-                    value={e.value}
-                    key={e.value}
-                    className="relative m-1 flex items-center justify-center rounded-lg px-4 py-2 pl-8 outline-none hover:bg-brandBlue focus:bg-brandBlue dark:hover:bg-brandBlue dark:focus:bg-brandBlue"
-                  >
-                    <DropdownMenu.ItemIndicator className="absolute -left-1 top-1/2 -translate-y-1/2">
-                      <BsDot size={36} />
-                    </DropdownMenu.ItemIndicator>
-                    <div className="flex w-full items-center justify-between gap-2">
-                      <span
-                        className={twMerge(
-                          "flex w-fit items-center font-semibold",
-                          e.disabled ? "text-textFadedLight dark:text-textFadedDark" : ""
+        <>
+          <div className="radio-dropdown-backdrop pointer-events-none absolute inset-0 bg-app-backdropUmbra-light transition dark:bg-app-backdropUmbra-dark sm:hidden"></div>
+          <div className={`${resolvedTheme} contents`}>
+            {/* bg-base-3-light dark:bg-base-3-dark w-[250px] text-black dark:text-white rounded-xl border-2 border-borders-base2-light dark:border-borders-base2-dark  */}
+            <DropdownMenu.Content
+              avoidCollisions
+              collisionPadding={16}
+              className="font-fancy radio-dropdown z-[9999] my-1 h-max w-[250px] cursor-default select-none overflow-hidden overflow-y-auto rounded-xl bg-base-3-light text-sm text-black transition-all dark:bg-base-3-dark dark:text-white"
+            >
+              <DropdownMenu.RadioGroup value={value} onValueChange={onValueChange}>
+                <div className="radio-dropdown-content-wrapper max-h-[var(--radix-popper-available-height)]">
+                  {formattedOptions.map((e) => (
+                    <DropdownMenu.RadioItem
+                      disabled={e.disabled}
+                      value={e.value}
+                      key={e.value}
+                      className="relative m-1 flex items-center justify-center rounded-lg px-4 py-2 pl-8 outline-none hover:bg-brandBlue focus:bg-brandBlue dark:hover:bg-brandBlue dark:focus:bg-brandBlue"
+                    >
+                      <DropdownMenu.ItemIndicator className="absolute -left-1 top-1/2 -translate-y-1/2">
+                        <BsDot size={36} />
+                      </DropdownMenu.ItemIndicator>
+                      <div className="flex w-full items-center justify-between gap-2">
+                        <span
+                          className={twMerge(
+                            "flex w-fit items-center font-semibold",
+                            e.disabled ? "text-textFadedLight dark:text-textFadedDark" : ""
+                          )}
+                        >
+                          {e.displayText}
+                        </span>
+                        {e.bubbleValue !== undefined && (
+                          <span className="font-semibold ">{e.bubbleValue}</span>
                         )}
-                      >
-                        {e.displayText}
-                      </span>
-                      {e.bubbleValue !== undefined && (
-                        <span className="font-semibold ">{e.bubbleValue}</span>
-                      )}
-                    </div>
-                  </DropdownMenu.RadioItem>
-                ))}
-              </div>
-            </DropdownMenu.RadioGroup>
-          </DropdownMenu.Content>
-        </div>
+                      </div>
+                    </DropdownMenu.RadioItem>
+                  ))}
+                </div>
+              </DropdownMenu.RadioGroup>
+            </DropdownMenu.Content>
+          </div>
+        </>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
   );
