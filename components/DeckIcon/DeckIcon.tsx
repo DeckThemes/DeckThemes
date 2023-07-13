e517d6792061aa95044d7d6f2cfcf8c4aadbd0ee
Evan / Fero <hi@evan.graphics>
2023-07-13 13:21:14 -0700

    fix(WEB-46): wrong icon colors on /download/deck

--- components/DeckIcon/DeckIcon.tsx
+++ components/DeckIcon/DeckIcon.tsx
@@ -2,7 +2,8 @@ import { useTheme } from "next-themes";
 
 export function DeckIcon({ iconName }: { iconName: string }) {
   const { resolvedTheme } = useTheme();
-  const iconSrc = `/deck-icons/${iconName}-${resolvedTheme}.svg`;
+  // I don't feel like renaming the icons, so we're bringing this back
+  const iconSrc = `/deck-icons/${iconName}-${resolvedTheme === "dark" ? "light" : "dark"}.svg`;
 
   // eslint-disable-next-line @next/next/no-img-element
   return <img className="max-w-4 mx-2 max-h-4" alt="Steam Deck icon" src={iconSrc} />;
