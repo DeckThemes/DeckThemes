edb4919df198b73cde96a963bd86b533a07c49cf
Beebles <102569435+beebls@users.noreply.github.com>
2023-08-02 19:24:15 -0600

    updated windows install instructions

--- pages/download/windows.tsx
+++ pages/download/windows.tsx
@@ -14,7 +14,6 @@ export default function WindowsDownload() {
           </span>{" "}
           for Windows
         </h1>
-
         {/* Blurb */}
         <p className="font-fancy mx-auto mt-6 max-w-2xl text-center text-sm font-medium leading-6 text-fore-10-light dark:text-fore-10-dark sm:text-lg">
           CSSLoader on Windows uses a native backend with a standalone frontend. You also install
@@ -23,7 +22,6 @@ export default function WindowsDownload() {
             Steam Deck
           </Link>
         </p>
-
         <InstallSection>
           <InstallSection.Header>Install CSSLoader Desktop</InstallSection.Header>
 
@@ -51,43 +49,101 @@ export default function WindowsDownload() {
                   launch.
                 </div>
               </li>
+              <li>
+                <div>
+                  Your antivirus may flag the backend executable, if so, please allow it to run.{" "}
+                  <a
+                    className="text-brandBlue hover:underline"
+                    href="https://github.com/suchmememanyskill/SDH-CssLoader"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    The source code
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    className="text-brandBlue hover:underline"
+                    href="https://github.com/suchmememanyskill/SDH-CssLoader/releases"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    released binaries
+                  </a>{" "}
+                  are both public for you to audit
+                </div>
+              </li>
 
               <li>
-                <div className="flex flex-row items-center">Restart Steam</div>
+                <div className="flex flex-col items-center gap-4">
+                  Windows contains an oddity that makes it impossible to create a symlink without
+                  elevated permissions or developer mode. CSS Loader requires a symlink to support
+                  local images or fonts.
+                  <br />
+                  Enable developer mode on Windows as follows:
+                  <ul className="flex w-full list-disc flex-col gap-4 pl-16">
+                    <li>Open Settings</li>
+                    <li>
+                      Select 'Privacy & Security' On Windows 10, this menu is called 'Updates &
+                      Security'
+                    </li>
+                    <li>Select 'For developers'</li>
+                    <li>Enable 'Developer Mode'</li>
+                    <li>
+                      If CSSLoader's backend is already installed, reboot your machine for this to
+                      take effect.
+                    </li>
+                    <li>Developer Mode may be disabled after one reboot</li>
+                  </ul>
+                </div>
               </li>
             </ol>
           </InstallSection.Content>
         </InstallSection>
-
         <InstallSection>
           <InstallSection.Header>Using CSSLoader Desktop</InstallSection.Header>
 
           <InstallSection.Content>
-            <div className="w-full">
-              Here's a quick rundown of how to use the CSSLoader Desktop app.
-            </div>
             <ul className="flex w-full list-disc flex-col gap-4">
               <li>
-                <div className="flex flex-row items-center">
-                  Manage your themes on the Themes page. Toggle them on or off using the switch, or
-                  customize them under the Theme Settings button.
+                <div>
+                  The <b>Themes</b> tab is where you can toggle and customize your installed themes.
+                  All installed themes are displayed here.
                 </div>
               </li>
 
               <li>
-                <div className="flex flex-row items-center">
-                  Explore all of our themes on the Store page.
+                <div>
+                  The <b>Store</b> is where you'll find new themes to download, browse and install
+                  at your will, and <b>Login with Token</b> to be able to star themes.
                 </div>
               </li>
 
               <li>
-                <div className="flex flex-row items-center">
-                  Login with your token on the Settings page to star themes.
+                <div>
+                  <b>Manage</b> contains buttons to update and delete your installed themes.
+                </div>
+              </li>
+
+              <li>
+                <div>
+                  <b>Settings</b> contains an input field for your DeckThemes login token, as well
+                  as additional settings for CSSLoader.
                 </div>
               </li>
             </ul>
           </InstallSection.Content>
         </InstallSection>
+        <span className="text-xl">
+          More info can be found on{" "}
+          <a
+            href="https://github.com/beebls/CSSLoader-Desktop#cssloader-desktop"
+            className="text-brandBlue hover:underline"
+            target="_blank"
+            rel="noreferrer"
+          >
+            CSSLoader Desktop's readme
+          </a>
+        </span>
       </div>
     </>
   );
