284d853a711fc56c3d57dca213062b820582ff67
Beebles <102569435+beebls@users.noreply.github.com>
2023-08-12 18:39:40 -0600

    one last fix

--- pages/submissions/view.tsx
+++ pages/submissions/view.tsx
@@ -41,79 +41,77 @@ export default function FullSubmissionViewPage() {
     return <LoadingPage />;
   }
 
+  if (!submissionData) {
+    return <span>Error! Invalid Submission ID</span>;
+  }
+
   return (
     <>
       <Head>
         <title>DeckThemes | Submission Viewer</title>
       </Head>
-      {submissionData ? (
-        <>
-          <div className="flex w-full flex-col items-center px-4">
-            <h1 className="mt-4 mb-4 text-3xl font-black md:text-6xl">
-              {submissionData.newTheme.type === "Audio"
-                ? FormattedSubmissionIntentAudio[submissionData.intent]
-                : FormattedSubmissionIntent[submissionData.intent]}
-            </h1>
-            <FullThemeCard parsedId={submissionData.newTheme.id} hideAdminMenu />
-          </div>
-          <div className="flex w-full flex-col items-center">
-            <h2 className="my-4 border-b-4 border-textLight text-3xl font-semibold dark:border-textDark md:text-4xl">
-              Submission Info
-            </h2>
-            <div className="mb-4 flex flex-col items-center">
-              {submissionData.errors && submissionData.errors.length > 0 ? (
-                <>
-                  <span className="text-2xl">Errors:</span>
-                  <div className="flex max-h-64 flex-col items-center overflow-y-scroll rounded-3xl border-4 border-borderLight px-8 text-center dark:border-borderDark">
-                    {submissionData.errors.map((e, i) => (
-                      <span key={`Submission Error ${i}`} className="max-w-[60vw]">
-                        <b>{i + 1}:</b> {e}
-                      </span>
-                    ))}
-                  </div>
-                </>
-              ) : (
-                <>
-                  <span>No Errors</span>
-                </>
-              )}
-            </div>
-            {submissionData.status === "AwaitingApproval" ? (
-              <>
-                <SubmissionReviewCard submissionData={submissionData} parsedId={parsedId} />
-              </>
-            ) : (
-              <>
-                <div className="flex w-full flex-col items-center pt-4">
-                  {submissionData.status !== "Dead" && (
-                    <div className="flex items-center gap-2 text-5xl">
-                      {submissionData.status === "Approved" ? (
-                        <BsCheckCircleFill className="text-emerald-600" />
-                      ) : (
-                        <BsXCircleFill className="text-red-500" />
-                      )}
-                      <span>{submissionData.status}</span>
-                    </div>
+      <div className="flex w-full flex-col items-center px-4">
+        <h1 className="mt-4 mb-4 text-3xl font-black md:text-6xl">
+          {submissionData.newTheme.type === "Audio"
+            ? FormattedSubmissionIntentAudio[submissionData.intent]
+            : FormattedSubmissionIntent[submissionData.intent]}
+        </h1>
+        <FullThemeCard parsedId={submissionData.newTheme.id} hideAdminMenu />
+      </div>
+      <div className="flex w-full flex-col items-center">
+        <h2 className="my-4 border-b-4 border-textLight text-3xl font-semibold dark:border-textDark md:text-4xl">
+          Submission Info
+        </h2>
+        <div className="mb-4 flex flex-col items-center">
+          {submissionData.errors && submissionData.errors.length > 0 ? (
+            <>
+              <span className="text-2xl">Errors:</span>
+              <div className="flex max-h-64 flex-col items-center overflow-y-scroll rounded-3xl border-4 border-borderLight px-8 text-center dark:border-borderDark">
+                {submissionData.errors.map((e, i) => (
+                  <span key={`Submission Error ${i}`} className="max-w-[60vw]">
+                    <b>{i + 1}:</b> {e}
+                  </span>
+                ))}
+              </div>
+            </>
+          ) : (
+            <>
+              <span>No Errors</span>
+            </>
+          )}
+        </div>
+        {submissionData.status === "AwaitingApproval" ? (
+          <>
+            <SubmissionReviewCard submissionData={submissionData} parsedId={parsedId} />
+          </>
+        ) : (
+          <>
+            <div className="flex w-full flex-col items-center pt-4">
+              {submissionData.status !== "Dead" && (
+                <div className="flex items-center gap-2 text-5xl">
+                  {submissionData.status === "Approved" ? (
+                    <BsCheckCircleFill className="text-emerald-600" />
+                  ) : (
+                    <BsXCircleFill className="text-red-500" />
                   )}
-                  <div className="mb-2 flex flex-col items-center">
-                    <span className="py-2 px-4 text-xl">{submissionData.message}</span>
-                  </div>
-                  {submissionData?.reviewedBy ? (
-                    <div className="flex items-center gap-2">
-                      <span>Reviewed By:</span>
-                      <div>
-                        <MiniPfpDisplay accountInfo={submissionData.reviewedBy} dark />
-                      </div>
-                    </div>
-                  ) : null}
+                  <span>{submissionData.status}</span>
                 </div>
-              </>
-            )}
-          </div>
-        </>
-      ) : (
-        <span>Error! Invalid Submission ID</span>
-      )}
+              )}
+              <div className="mb-2 flex flex-col items-center">
+                <span className="py-2 px-4 text-xl">{submissionData.message}</span>
+              </div>
+              {submissionData?.reviewedBy ? (
+                <div className="flex items-center gap-2">
+                  <span>Reviewed By:</span>
+                  <div>
+                    <MiniPfpDisplay accountInfo={submissionData.reviewedBy} dark />
+                  </div>
+                </div>
+              ) : null}
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
