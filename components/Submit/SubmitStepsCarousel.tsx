0e3195e4370534d6a4bf8352755070509dcbe046
Beebles <102569435+beebls@users.noreply.github.com>
2023-08-21 17:01:21 -0600

    add disabled state to submit button

--- components/Submit/SubmitStepsCarousel.tsx
+++ components/Submit/SubmitStepsCarousel.tsx
@@ -134,7 +134,7 @@ export function SubmitStepsCarousel({
             </button>
             <button
               disabled={currentStep === 3 && !checkIfReady()}
-              className="group mb-2 inline-flex w-fit items-center justify-center gap-2 rounded-full border-2 border-borders-base1-light bg-brandBlue py-2 px-4 text-sm font-semibold text-white no-underline transition hover:border-borders-base2-light hover:bg-fore-11-dark hover:text-fore-contrast-dark focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 active:opacity-60 dark:border-borders-base1-dark hover:dark:border-borders-base2-dark sm:mb-0"
+              className="group mb-2 inline-flex w-fit items-center justify-center gap-2 rounded-full border-2 border-borders-base1-light bg-brandBlue py-2 px-4 text-sm font-semibold text-white no-underline transition hover:border-borders-base2-light hover:bg-fore-11-dark hover:text-fore-contrast-dark focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 active:opacity-60 disabled:pointer-events-none disabled:opacity-50 dark:border-borders-base1-dark hover:dark:border-borders-base2-dark sm:mb-0"
               onClick={() => (currentStep !== 3 ? goToNextStep() : formatDataForSubmission())}
             >
               {currentStep !== 3 ? "Next" : "Submit"}
